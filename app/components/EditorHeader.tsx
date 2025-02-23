d6ccccbcee92cef89c114db70146fca17af3ef7e
Jianganchen <jianganchen@ufl.edu>
2025-02-23 04:58:57 -0500

    Fixed toggle edit button & toggle MCQ button

--- app/components/EditorHeader.tsx
+++ app/components/EditorHeader.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Editor } from "@tiptap/core";
+import { Edit, Eye, ListChecks } from "lucide-react";
 import { useCallback } from "react";
 
 export type EditorHeaderProps = {
@@ -18,7 +19,31 @@ export const EditorHeader = ({ editor }: EditorHeaderProps) => {
     <div className="flex flex-row items-center justify-between flex-none py-2 pl-6 pr-3 text-black bg-white border-b border-neutral-200 dark:bg-black dark:text-white dark:border-neutral-800">
       <div className="flex flex-row gap-x-1.5 items-center">
         <div className="flex items-center gap-x-1.5">
-          <button onClick={toggleEditable}>Toggle Editable</button>
+          {/* Toggle Edit/Read-only Button */}
+          <button
+            onClick={toggleEditable}
+            className="flex items-center justify-center w-10 h-10 rounded-full transition-all text-buttonColor hover:text-yellow-500"
+          >
+            {editor.isEditable ? (
+              <Eye className="w-6 h-6" />
+            ) : (
+              <Edit className="w-6 h-6" />
+            )}
+          </button>
+
+          {/* Toggle MCQ Button */}
+          {editor.isEditable && (
+            <button
+              onClick={() => editor?.chain().focus().toggleMCQ().run()}
+              className={`flex items-center justify-center w-10 h-10 rounded-full transition-all ${
+                editor.isActive("mcq")
+                  ? "text-buttonColor hover:text-yellow-500"
+                  : "text-textColor hover:text-d1d0c5"
+              }`}
+            >
+              <ListChecks className="w-6 h-6" />
+            </button>
+          )}
         </div>
       </div>
     </div>
--- app/components/Tiptap.tsx
+++ app/components/Tiptap.tsx
@@ -22,43 +22,12 @@ const Tiptap = ({ editor, isEditable }: EditorHeaderProps) => {
       .updateAttributes("mcq", {
         isEditable: isEditable,
       })
+      .blur()
       .run();
   }, [isEditable]);
 
-  const toggleEditable = () => {
-    editor.setOptions({ editable: !editor.isEditable });
-    // force update the editor
-    editor.view.dispatch(editor.view.state.tr);
-  };
-
   return (
     <div className="max-w-3xl mx-auto p-6 bg-background text-textColor shadow-lg rounded-lg">
-      <div className="flex items-center gap-1 mb-4">
-        {/* Toggle Edit/Read-only Button */}
-        <button
-          onClick={toggleEditable}
-          className="flex items-center justify-center w-10 h-10 rounded-full transition-all text-buttonColor hover:text-yellow-500"
-        >
-          {isEditable ? (
-            <Eye className="w-6 h-6" />
-          ) : (
-            <Edit className="w-6 h-6" />
-          )}
-        </button>
-
-        {/* Toggle MCQ Button */}
-        <button
-          onClick={() => editor?.chain().focus().toggleMCQ().run()}
-          className={`flex items-center justify-center w-10 h-10 rounded-full transition-all ${
-            editor.isActive("mcq")
-              ? "text-buttonColor hover:text-yellow-500"
-              : "text-textColor hover:text-d1d0c5"
-          }`}
-        >
-          <ListChecks className="w-6 h-6" />
-        </button>
-      </div>
-
       <EditorContent
         editor={editor}
         className="border border-secondary p-4 rounded-md shadow-sm focus:outline-none focus:ring-0 text-xl font-semibold leading-10"
